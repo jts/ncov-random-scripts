b3ef2d804ea8e8942afdc07b86817686e1e52450
Jared Simpson <jared.simpson@gmail.com>
2020-12-23 16:27:28 -0500

    also look for uncompressed vcf (#2)

--- ncov-watch.py
+++ ncov-watch.py
@@ -55,7 +55,7 @@ def load_ivar_variants(filename):
 
 def get_from_directory():
     find_files = lambda pattern : [ path for path in Path(args.directory).rglob(pattern) ]
-    files = find_files("*pass.vcf.gz") + find_files("*variants.tsv")
+    files = find_files("*pass.vcf") + find_files("*pass.vcf.gz") + find_files("*variants.tsv")
     for f in files:
         yield str(f)
 
